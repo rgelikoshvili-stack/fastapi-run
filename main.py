e4fd06d2f0d5fa146e856fac9d7219af38648af0
rgelikoshvili <r_gelikoshvili@gmail.com>
2026-03-06 18:19:46 +0000

    Sprint 28: Fix import error in main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-import os, UploadFile, File
+import os
 from datetime import datetime, timezone
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address

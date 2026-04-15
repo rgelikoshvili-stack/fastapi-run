0a0c34b6145815e491ac15d2ec0ec9b24b156192
ROLANDI GELIKOSHVILI <rolandi@example.com>
2026-04-15 21:03:01 +0400

    chore: remove temporary debug and fix scripts

---
+++ remove_all_feff.py
@@ -0,0 +1,12 @@
+﻿p = 'app/api/services/llm_service.py'
+
+c = open(p, 'r', encoding='utf-8-sig').read()
+
+# ყველა BOM/non-printable FEFF მოვაშოროთ
+c = c.replace('\ufeff', '')
+
+with open(p, 'w', encoding='utf-8', newline='\n') as f:
+    f.write(c)
+
+print('ALL FEFF REMOVED')
+print(repr(open(p, 'r', encoding='utf-8').read()[:40]))
